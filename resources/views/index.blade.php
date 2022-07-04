3c006b39cf07f2aa8ed17bd3b0af21ba5e8aadd3
Irfan Saputra <irfan_saputra96@hotmail.com>
2022-07-04 16:18:24 +0700

    changed phone number

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -476,7 +476,7 @@
               <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
               <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
               <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
-              <a href="https://api.whatsapp.com/send?phone=6285156626308" class="whatsapp"><i class="bx bxl-whatsapp"></i></a>
+              <a href="https://api.whatsapp.com/send?phone=6281288472408" class="whatsapp"><i class="bx bxl-whatsapp"></i></a>
               <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
             </div>
               <!-- <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621" frameborder="0" style="border:0; width: 100%; height: 290px;" allowfullscreen></iframe> -->
