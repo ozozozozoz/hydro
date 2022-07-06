0c729550a8609fc970d35d2cf587041d718e70eb
Irfan Saputra <irfan_saputra96@hotmail.com>
2022-07-06 15:17:11 +0700

    Project 1 page modified

--- resources/views/ourproject1.blade.php
+++ resources/views/ourproject1.blade.php
@@ -11,22 +11,44 @@
 @section('content')
 
 <div class="our-project-div">
-    <img src="assets/img/1.jpg">
+    <img src="assets/img/1.jpg" style="width: 700px; height: 430px;justify-content:center;">
 </div>
 <br/>
 <div class="project-container">
     <div class="left-txt">
         <h4>DESCRIPTION</h4>
+        <p>This is a past project conducted by us</p>
         <h4>WHEN</h4>
+        <p>6th July 2022</p>
         <h4>WHO</h4>
+        <p>PT. ABC Tbk.</p>
     
     </div>
     
-    <div class="right txt">
+    <div class="right-txt">
         <p>Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. 
             Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit. Nam nec tellus a 
-            odio tincidunt auctor a ornare odio. Sed non mauris vitae erat consequat.</p>
+            odio tincidunt auctor a ornare odio. Sed non mauris vitae erat consequat. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. 
+            Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit. Nam nec tellus a 
+            odio tincidunt auctor a ornare odio. Sed non mauris vitae erat consequat.<br><br>
+            Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. 
+            Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit. Nam nec tellus a 
+            odio tincidunt auctor a ornare odio. Sed non mauris vitae erat consequat. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. 
+            Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit. Nam nec tellus a 
+            odio tincidunt auctor a ornare odio. Sed non mauris vitae erat consequat.<br><br>
+            Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. 
+            Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit. Nam nec tellus a 
+            odio tincidunt auctor a ornare odio. Sed non mauris vitae erat consequat. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem nibh id elit. 
+            Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi accumsan ipsum velit. Nam nec tellus a 
+            odio tincidunt auctor a ornare odio. Sed non mauris vitae erat consequat.
+        </p>
     </div>
+</div>
+
+<div class="project-photo">
+    <img src="assets/img/1.jpg" style="width: 800px; height: 430px;justify-content:center;">
+    <img src="assets/img/1.jpg" style="width: 800px; height: 430px;justify-content:center;">
+    <img src="assets/img/1.jpg" style="width: 800px; height: 430px;justify-content:center;">
 </div>
 
 <hr>
